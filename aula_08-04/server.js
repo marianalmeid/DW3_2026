dee6b391799bcbcfb8ac9730719357d50afd8f22
Mariana <163425141+marianalmeid@users.noreply.github.com>
2026-04-08 11:51:56 -0300

    aula_08-04

---
+++ aula_08-04/server.js
@@ -0,0 +1,34 @@
+import Fastify from 'fastify'
+import cors from '@fastify/cors'
+
+import tarefaRoutes from './src/routes/tarefa.routes.js'
+
+const server = Fastify()
+
+
+server.register(cors, {
+    origin: '*',
+    methods: ['GET', 'POST', 'PATCH', 'DELETE', 'OPTIONS']
+})
+
+server.register(tarefaRoutes)
+
+server.setNotFoundHandler((request, reply) => {
+  reply.code(404).send({
+    status: 'error',
+    message: 'O recurso solicitado não existe nesta API.',
+  })
+})
+
+const PORT = 3000
+const start = async () => {
+    try {
+        await server.listen({port: PORT})
+        console.log(`Servidor rodando em http://localhost:${PORT}`)
+    } catch (erro) {
+        console.error(erro)
+        process.exit(1)
+    }
+}
+
+start()
---
+++ aula_08-04/src/routes/tarefa.routes.js
@@ -0,0 +1,51 @@
+// @file: src/ROUTES/tarefa.routes.js
+
+import { atualizarTarefa, concluirTarefa, criarTarefa, listarTarefas, obterResumo, obterTarefa, removerTarefa } from '../controllers/tarefa.controller.js'
+
+export default async function tarefaRoutes(server, options) {
+
+
+    server.get('/tarefas', async (request, reply) => {
+        // LOG para indicar que a rota foi chamada
+        console.log("Routes: GET /tarefas chamada");
+        // Chama a função do controlador para processar a requisição
+        listarTarefas(request, reply)
+    })
+
+    server.post('/tarefas', async (request, reply) => {
+       // log
+       console.log("Routes: POST /tarefa criada");
+
+       criarTarefa(request, reply)
+    })
+
+    server.get('/tarefas/resumo', async (request, reply) => {
+       console.log("Routes: GET / resumo das tarefas");
+
+       obterResumo(request, reply)
+    })
+
+  server.get('/tarefas/:id', async (request, reply) => {
+        console.log("Routes: GET /tareaf obtida");
+
+        obterTarefa(request, reply)
+  })
+
+  server.patch('/tarefas/:id', async (request, reply) => {
+        console.log("Routes: PATCH / tarefa ataualizada");
+
+        atualizarTarefa(request, reply)
+  })
+
+  server.patch('/tarefas/:id/concluir', async (request, reply) => {
+    console.log("Routes: PATCH / tarefa concluida");
+
+    concluirTarefa(request, reply)
+  })
+
+  server.delete('/tarefas/:id', async (request, reply) => {
+    console.log("Routes: DELETE / tarefa deletada");
+
+    removerTarefa(request, reply)
+  })
+}
